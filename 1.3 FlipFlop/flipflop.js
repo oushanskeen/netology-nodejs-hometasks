9a5cee9356277983aeae81fc7e8554c4f2678cbf
oushanskeen <inta.soul@gmail.com>
2021-02-17 01:17:47 +0300

    Hope all mistakes fixed

--- 1.3 FlipFlop/flipflop.js
+++ 1.3 FlipFlop/flipflop.js
@@ -3,24 +3,30 @@ const path = require("path");
 const argv = process.argv;
 const readline = require("readline");
 const input = readline.createInterface(process.stdin);
-const data = require("./flipFlopData.json");
-const dataFile = path.join(__dirname, "flipFlopData.json");
-console.log("JSON READ: ", data);
+//const db = require(`./${argv[2]}`);
 
-const generateFlipflop = () => +Math.ceil(Math.random() * 2);
+const generateFlipflop = () => Date.now()%2 == 0 ? 1 : 2;
+const dataFile = filename => path.join(__dirname, filename);
 
 console.log("Орёл(1) или Решка(2) ?");
 
-const guess = generateFlipflop();
 input.prompt();
 input.on("line", data => {
-  const situation = +data === guess ? "Win!" : "Lost!";
-  //console.log(situation);
-  //console.log("guess", guess);
-  const record = [guess, data, situation];
-  const newDataObj = {...data, [Date.now()]:record};
-  fs.writeFile(dataFile,JSON.stringify(newDataObj),(err)=> console.log(err ? "err":":)"));
-  console.log(`You ${situation}`);
-  input.close();
-})
-
+  const filename = argv[2];
+  fs.readFile(filename, "utf-8", (err, db) => {
+    //err 
+    // ? console.log("You need to create new file")
+    // : console.log("Data from fs read file", db);
+    const DB = db ? JSON.parse(db) : {};
+    const guess = generateFlipflop();
+    const situation = +data === guess ? "Win!" : "Lost!";
+    const record = [guess, data, situation];
+    const newDataObj = { ...DB, [Date.now()]: record };
+  
+    fs.writeFile(`./${argv[2]}`, JSON.stringify(newDataObj), err =>
+      console.log(err ? "err" : ":)")
+    );
+    console.log(`You ${situation}`);
+    input.close();
+  });
+});
